8abcac5860a8856f5faad29b2a815632fd38b7d8
Rynav <rynav@rynav.xyz>
2024-10-12 17:27:32 +0200

    feat: Added build script for docker.

---
+++ build.sh
@@ -0,0 +1,15 @@
+echo "Pulling new code..."
+
+git pull
+
+echo "Building new image..."
+
+docker build -t picosong.site.api .
+
+echo "Removing old docker image and creating new one"
+
+docker stop picosong.api
+docker rm picosong.api
+docker run -d --network host -v /mnt/pico/files:/app/files/files:ro -v /home/pico/allfiles.db:/app/src/db/allfiles.db:ro -v /home/pico/songs.sqlite:/app/src/db/songs.sqlite:ro --name picosong.api picosong.site.api
+
+echo "Done!"
